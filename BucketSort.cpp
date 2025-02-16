3e2ba5389aab6614db26b82093d8f42981908ac4
Pratheesh-555 <127003195@sastra.ac.in>
2025-02-16 20:06:27 +0530

    included count variable

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -2,54 +2,59 @@
 
 using namespace std;
 
-class Node {
-public:
-    int data;
-    Node* next;
-
-    Node(int data) {
-        this->data = data;
-        this->next = nullptr;
-    }
-};
-
-class Bucket {
-public:
-    Node* head;
-
-    Bucket() {
-        head = nullptr;
-    }
-
-    void insertOrder(int value) {
-        Node* newNode = new Node(value);
-
-        if (!head || value < head->data) {
-            newNode->next = head;
-            head = newNode;
-            return;
-        }
-
-        Node* current = head;
-        while (current->next && current->next->data < value) {
-            current = current->next;
-        }
-        newNode->next = current->next;
-        current->next = newNode;
-    }
-
-    void getSortedValues(int A[], int& index) {
-        Node* current = head;
-        while (current) {
-            A[index++] = current->data;
-            current = current->next;
-        }
-    }
-};
+long int count = 0;
 
 class BucketSort {
+private:
+    class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node(int data) {
+            this->data = data;
+            this->next = nullptr;
+        }
+    };
+
+    class Bucket {
+    public:
+        Node* head;
+
+        Bucket() {
+            head = nullptr;
+        }
+
+        void insertOrder(int value) {
+            Node* newNode = new Node(value);
+
+            if (!head || value < head->data) {
+                newNode->next = head;
+                head = newNode;
+                return;
+            }
+
+            Node* current = head;
+            while (current->next && current->next->data < value) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+
+        void getSortedValues(int A[], int& index) {
+            Node* current = head;
+            while (current) {
+                A[index++] = current->data;
+                current = current->next;
+            }
+        }
+    };
+
 public:
     void bucketSort(int A[], int n) {
+        count++; // Counting function call
+
         if (n <= 0) return;
 
         int maxValue = A[0];
@@ -58,6 +63,7 @@ public:
                 maxValue = A[i];
             }
         }
+        count++; // Counting max finding loop
 
         int bucketCount = 10;
         Bucket* buckets = new Bucket[bucketCount];
@@ -66,11 +72,13 @@ public:
             int index = (A[i] * bucketCount) / (maxValue + 1);
             buckets[index].insertOrder(A[i]);
         }
+        count++; // Counting bucket insertion loop
 
         int index = 0;
         for (int i = 0; i < bucketCount; i++) {
             buckets[i].getSortedValues(A, index);
         }
+        count++; // Counting sorting retrieval loop
 
         delete[] buckets;
     }
@@ -95,6 +103,8 @@ int main() {
         cout << A[i] << " ";
     }
     cout << endl;
+
+    cout << "Count: " << count << endl;
 
     delete[] A;
     return 0;
